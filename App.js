16bddea8776e98dd51bb33ab3bcf7aab3a372250
Hanna-Mari Kielenniva <h.kielenniva@gmail.com>
2024-11-09 18:35:20 +0200

    Työstetty App ja Login komponentteja, lisätty Signup ja Map komponentit
    App komponenttiin lisätty:
    -tarkistus onko käyttäjä kirjautunut sisään
    - stack navigointi
    
    Login komponenttiin lisätty:
    - funktio jolla käsitellään kirjautumista
    - navigointi signup-komponenttiin

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createStackNavigator } from '@react-navigation/stack'; 
+import { onAuthStateChanged } from 'firebase/auth'; 
+import { auth } from './components/firebase';
 
-export default function App() {
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Map from './components/Map';
+
+
+
+const Stack = createStackNavigator(); 
+
+const App = () => {
+  const [user, setUser] = useState(null);  // Käyttäjän tilan hallinta
+  
+
+  useEffect(() => {
+    // Tarkistetaan, onko käyttäjä kirjautunut
+    const unsubscribe = onAuthStateChanged(auth, (user) => { //firebase Authenticationin kuuntelija, joka odottaa käyttäjän tilan muutoksia (sisään/uloskirjautuminen)
+      if (user) {
+        setUser(user);  // Käyttäjä on kirjautunut sisään, asetetaan user tilaan käyttäjän tiedot
+      } else {
+        setUser(null);  // Käyttäjä ei ole kirjautunut
+      }
+    });
+
+    // Poistetaan kuuntelija, jotta se ei jää aktiiviseksi taustalle
+    return () => unsubscribe();
+  }, []); //tyhjä lista, kirjautuminen tarkastetaan vain kerran 
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>  
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="Map" component={Map} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
--- components/firebase.js
+++ components/firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"; // kirjautuminen ja registeröityminen
-import firebase from "firebase/compat/app";
 import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore"; // tietojen tallentaminen ja hakeminen tietkokannsta 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,8 +18,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(); 
-const db = getFirestore();
+const auth = getAuth(app); 
+const db = getFirestore(app);
 
 
 
@@ -74,3 +73,5 @@ const saveObservation = async (observation) => { //tallentaa uuden havainnon
         console.error("Error getting documents: ", e);
     }
 }; //Get multiple documents from a collection avulla pitäisi näkyä kaikki havainnot kartalla, ei vain yksi havainto
+
+export { auth, db };
